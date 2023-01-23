026b6b2b3ac42707b7b6c30f10043fdc37da461f
Cassandra Keddis <cassandra@thinkific.com>
2023-01-22 16:30:35 -0800

    modify state with click event

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 
-function LightSwitchButton(props){
+function LightSwitchButton(props) {
   const [light, setLight] = useState('off');
+  const handleClick = () => setLight(light === "on" ? "off" : "on");
 
   return (
-    <button className="LightSwitchButton">
-      {light === "on" && <span><i>💡</i> I'm on!</span>}
-      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
+    <button onClick={handleClick} className="LightSwitchButton">
+      {light === 'on' && <span><i>💡</i> I'm on!</span>}
+      {light === 'off' && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
   );
 }
